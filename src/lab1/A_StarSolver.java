db69d1b8c497c99525a86b06d5623ff08c6e610f
Basel Saad <bsaad04@campus.haifa.ac.il>
2018-03-20 11:48:41 +0200

    stam

--- src/lab1/A_StarSolver.java
+++ src/lab1/A_StarSolver.java
@@ -13,7 +13,7 @@ import java.util.Map;
 	(5) If i is a goal node, exit with success; a solution has been found.
 	(6) Expand node i, creating nodes for all of its successors. For every successor node j of i:
 	
-		a. Calculate f(j)- DONE!!!
+		a. Calculate f(j)- DONE!!!!
 		
 		b. If j is neither in OPEN nor in CLOSED, then add it to OPEN with its f value. 
 				Attach a pointer from j back to its predecessor i 
@@ -150,7 +150,7 @@ public class A_StarSolver {
 	**/
 	private void calculate_Heuristics(State node) {
 		if(node.isGoal()){
-			node.setHValue(-1.0);
+			node.setHValue(-100.0);
 		}else{
 			node.setHValue(0);
 		}
@@ -173,7 +173,7 @@ public class A_StarSolver {
 	
 	public static void main(String[] args){
 		State s = new State();
-		s.initilizeState("AA...OP..Q.OPXXQ.OP..Q..B...CCB.RRR.");
+		s.initilizeState("AA...........XX.....................");
 		s.setParent(null);
 		s.show();
 		A_StarSolver solver = new A_StarSolver(s);
