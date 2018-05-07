605a6c68c4ac36306ea170c65ffc13d8e55e3a46
Basel Saad <bsaad04@campus.haifa.ac.il>
2018-05-07 21:31:44 +0300

    fixed minimal conflicts

--- GraphColoring/main.cpp
+++ GraphColoring/main.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
-#include "local_search.h"
+#include "graph.h"
 
 
 int main(){
-    std::string path = "inputs/le450_15a.col";
+    std::string path = "inputs/le450_5c.col";
     Graph g1;
     g1.readGraph(path);
     g1.greedyColoring();
     //std::cout << "100" << std::endl;
-    g1.reduceColors();
+    g1.reduceColors(0);
     //std::cout << "111" << std::endl;
 
 
-    std::cout << g1.minimalConflicts() << std::endl;
+    //std::cout <<  << std::endl;
 
     return 0;
 
